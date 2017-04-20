687b6357d63fe2994939c96196e78e68d720a2a4
anhhtle <anh.ht.le@gmail.com>
2017-04-19 17:45:49 -0700

    CORS

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ const userRouter = require('./router/userRouter');
 require('dotenv').config();
 
 const app = express();
+
+// alow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -20,6 +29,7 @@ mongoose.Promise = global.Promise;
 
 // router
 app.use('/user', userRouter);
+
 
 
 // catch all
